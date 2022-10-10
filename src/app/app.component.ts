64aa9bb42926e3bd0d5d24f0df802ae625730cbc
Pedro J. Molina <pjmolina@gmail.com>
2022-10-10 10:19:39 +0200

    ejemplo de bucle for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './user/user.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular0';
+  usuarioSeleccionado = '';
+
+  personas: Persona[] = [
+    {
+      name: 'Pedro',
+      surname: 'Molina',
+    },
+    {
+      name: 'Manuel',
+      surname: 'Alvarez',
+    },
+    {
+      name: 'Ana',
+      surname: 'Perez',
+    },
+    {
+      name: 'Alicia',
+      surname: 'Gomez',
+    },
+    {
+      name: 'Zacarias',
+      surname: 'Gomez',
+    },
+  ];
+
+  onSelect(persona: Persona): void {
+    console.log('Componente padre. seleccionado: ' + persona.name);
+    this.usuarioSeleccionado = persona.name;
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+
+export interface Persona {
+  name: string;
+  surname: string;
+}
+
+let seq = 0;
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent implements OnInit {
-  constructor() {}
+export class UserComponent implements OnInit, OnDestroy, OnChanges {
+  @Input() name?: string = '';
+  @Input() surname: string = '';
+  @Output() seleccionado = new EventEmitter<Persona>();
 
-  ngOnInit(): void {}
+  private idSeq: number = 0;
+
+  constructor() {
+    this.idSeq = seq++;
+    console.log('Construyendo componente user ' + this.idSeq);
+  }
+
+  ngOnInit(): void {
+    console.log('Inicializando componente user ' + this.idSeq);
+  }
+  ngOnDestroy(): void {
+    console.log('Destuyendo componente user ' + this.idSeq);
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['name']) {
+      //
+    }
+
+    console.log(changes);
+  }
+
+  pulsado(): void {
+    console.log('Pulsado en  ' + this.name);
+
+    this.seleccionado.emit({
+      name: this.name || '',
+      surname: this.surname,
+    });
+  }
 }
