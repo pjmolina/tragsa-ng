8a43a6f088c9bba2fe040c3b7dee9225dc310e1b
Pedro J. Molina <pjmolina@gmail.com>
2022-10-10 13:22:44 +0200

    doblew datab-binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [AppComponent, UserComponent, WeatherComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [
     // 1. inyectar directamente la clase
     // LoggerService
