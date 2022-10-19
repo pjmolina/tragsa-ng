e664075915ecc1213131b5b949ea749614272199
Pedro J. Molina <pjmolina@gmail.com>
2022-10-19 13:42:32 +0200

    service test

--- src/app/planet/planet.service.spec.ts
+++ src/app/planet/planet.service.spec.ts
@@ -1,16 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { Observable, of } from 'rxjs';
+import { PlanetResponse } from './planet';
 
 import { PlanetService } from './planet.service';
 
-xdescribe('PlanetService', () => {
+class MockHttpClient {
+  get(): Observable<Partial<PlanetResponse>> {
+    return of({
+      name: 'ejemplo',
+      population: '100',
+      orbital_period: '3',
+    } as Partial<PlanetResponse>);
+  }
+}
+
+describe('PlanetService', () => {
   let service: PlanetService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: HttpClient, useClass: MockHttpClient }],
+    });
     service = TestBed.inject(PlanetService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('getPlanet()', () => {
+    const planet$ = service.getPlanet(7);
+    expect(planet$).toBeTruthy();
   });
 });
